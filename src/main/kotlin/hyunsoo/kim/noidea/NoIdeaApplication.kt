ffcc491aa640db207359942a7b876bbdbb95f82e
kim.hyunsoo.96.kr <kim.hyunsoo.96.kr@gmail.com>
2024-09-23 23:34:58 +0900

    no idea init

---
+++ src/main/kotlin/hyunsoo/kim/noidea/NoIdeaApplication.kt
@@ -0,0 +1,11 @@
+package hyunsoo.kim.noidea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NoIdeaApplication
+
+fun main(args: Array<String>) {
+    runApplication<NoIdeaApplication>(*args)
+}
---
+++ src/test/kotlin/hyunsoo/kim/noidea/NoIdeaApplicationTests.kt
@@ -0,0 +1,13 @@
+package hyunsoo.kim.noidea
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoIdeaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
